69dfa0be5da570d8e8f2346c5c9aa77ca8e4a60c
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-26 18:37:56 +0700

    if the value in the data field of the new node is greater than that of the parent

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Node
 {
 public:
+    int info;
     Node *leftchild;
     Node *rightchild;
 
@@ -22,7 +23,7 @@ public:
 
     BinaryTree()
     {
-        ROOT = nullptr; //Initializing ROOT to null
+        ROOT = nullptr; // Initializing ROOT to null
     }
 
     void insert()
@@ -31,41 +32,40 @@ public:
         cout << "Masukan nilai :";
         cin >> x;
 
-        //1. allocate memory foor the new node
+        // 1. allocate memory foor the new node
         Node *newNode = new Node();
 
-        //2. Assign value to the data field of new node
-        newNode -> info = x;
+        // 2. Assign value to the data field of new node
+        newNode->info = x;
 
-        //3. Make the left and right child of the new node point to null
-           newNode ->leftchild = nullptr;
-        newNode ->rightchild = nullptr;
+        // 3. Make the left and right child of the new node point to null
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
 
-        //4.locate the node which will be the parent of the node to be inserted
-        Node * parent = nullptr;
-        Node * currentNode = nullptr;
-        search (x, parent, currentNode);
-        
-        //5. if parent is NULL (tree is empty)
-        if  (parent == nullptr)
+        // 4.locate the node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
+
+        // 5. if parent is NULL (tree is empty)
+        if (parent == nullptr)
         {
-            //5a. mark the new node as ROOT
+            // 5a. mark the new node as ROOT
             ROOT = newNode;
-            
-            //5b. Exit
+
+            // 5b. Exit
             return;
         }
 
-        //6. if the value in data field of new node is less than that of parent
-        if (x < parent -> info)
+        // 6. if the value in data field of new node is less than that of parent
+        if (x < parent->info)
         {
-            //6a. make the left child of parent point to the new node
-            parent -> leftchild = newNode;
-            
-            //6b. exit
-            return;
-        }
+            // 6a. make the left child of parent point to the new node
+            parent->leftchild = newNode;
 
-     
+            // 6b. exit
+            return;
     }
+
+
 };
