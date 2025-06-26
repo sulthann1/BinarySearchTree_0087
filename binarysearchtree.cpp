1868e683b7768b563eae708d300824116a20a089
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-26 18:48:21 +0700

    menambahkan boolean

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -80,6 +80,24 @@ public:
 
     void search (int element, Node *&parent, Node *&currentNode)
     {
-        
+      //this function searches the currentNode of the specified node as well as the current node of its parent
+      currentNode = ROOT;
+      parent = nullptr;
+      while ((currentNode !=nullptr) && (currentNode -> info != element))
+      {
+        parent = currentNode;
+        if  (element < currentNode -> info)
+        currentNode = currentNode ->leftchild;
+        else
+        currentNode = currentNode ->rightchild;
+      }
+    
     }
+
+    bool isEmpty()
+    {
+        //check if the tree is empty
+        return ROOT ==nullptr;
+    }
+    
 };
