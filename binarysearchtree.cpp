1b6d4af99a290112761c39f9432d8f2ed939a05a
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-26 18:31:51 +0700

    if the value in data field of new node is less than that of parent make the left child of parent point to the new node

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -56,6 +56,16 @@ public:
             return;
         }
 
+        //6. if the value in data field of new node is less than that of parent
+        if (x < parent -> info)
+        {
+            //6a. make the left child of parent point to the new node
+            parent -> leftchild = newNode;
+            
+            //6b. exit
+            return;
+        }
+
      
     }
 };
